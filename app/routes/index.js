c9ba2cb58e33c7b522cb7235bc486d9df8b20840
JamesIrwin3 <139516801+JamesIrwin3@users.noreply.github.com>
2023-10-29 15:26:17 -0400

    added put and delete methods.

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,37 +1,87 @@
 const express = require("express");
 const router = express.Router();
+const { v4 } = require("uuid");
 
 //most storage methods will utilize uuid or ulid to index records this will be able to mimick that behavior better then an array
 var users = {}; 
 
 //GET - localhost:3000/api
 router.get("/", (req, res) => res.status(200).json({
-	message: "GET - API",
-	metadata: {
-		hostname: req.hostname,
-		method: req.method
-	}
+	users
 }));
 
 //GET - localhost:3000/api/:id
 router.get("/:id", (req, res) => {
 	const { id } = req.params
+	const user = users[id];
+
+	if (!user){
+		//Respond with not found
+		return res.status(404).json({
+			message: "User not found",
+			id
+		});
+	}
 
 	res.status(200).json({
-		message: "POST - API with id",
 		id,
+		user
 	});
-	
 });
 
 //POST - localhost:3000/api
 router.post("/", (req, res) => {
-	const { data } = req.body;
-	console.log(data);
-	res.status(200).json({
+	const body = req.body;
+	const id = v4();
+	//Insert request body
+	//TODO: - Data validation
+	users[id] = body
+	res.status(201).json({
 		message: "POST - API",
-		data: data
-	})
+		id,
+	});
+});
+
+//PUT - localhost:3000/api/:id
+router.put("/:id", (req, res) => {
+	const { id } = req.params;
+	const user = users[id];
+
+	if(!user){
+		return res.status(404).json({
+			message: "User not found",
+			id
+		});
+	}
+
+	//Update the user
+	//TODO: Data Validation
+	users[id] = {...user, ...req.body}; //combine the two objects with req.body taking precedence of overlapping information.
+
+	//Report success
+	res.status(200).json({
+		message: "User updated",
+		id,
+		user: users[id] //refetch the user
+	});
+});
+
+//DELETE - localhost:3000/api/:id
+router.delete("/:id", (req, res) => {
+	const { id } = req.params;
+	const user = users[id];
+
+	if (!user) {
+		return res.status(404).json({
+			message: "User not found",
+			id
+		});
+	}
+
+	res.status(204).json({
+		message: "User deleted",
+		id
+	});
 });
 
 module.exports = router;
