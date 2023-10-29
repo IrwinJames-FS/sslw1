9de904506aad220a334bbdb564111170a153c371
JamesIrwin3 <139516801+JamesIrwin3@users.noreply.github.com>
2023-10-29 15:33:54 -0400

    Updated inital entry point localhost:3000

--- app/index.js
+++ app/index.js
@@ -8,14 +8,12 @@ const router = require("./routes");
 
 app.use(express.json());
 
-app.get("/", (req, res) => res.status(200).json({
-	message: "GET to API",
-	metadata: {
-		hostname: req.hostname,
-		method: req.method
-	}
-}));
+app.get("/", (req, res) => {
+	res.status(200);
+	res.setHeader("Content-Type", "text/html");
+	res.write("<h1>Service is up</h1>")
+});
 
-app.use("/api", router);
+app.use("/users", router);
 
 module.exports = app;
